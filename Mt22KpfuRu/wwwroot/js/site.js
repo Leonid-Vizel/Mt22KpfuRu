cce1ba092d46524d03b4a7bef964683b08b0b1da
Leonid-Vizel <spartanlaska@gmail.com>
2022-10-27 10:41:55 +0300

    [Update] > Сделана страница регистрации #3 > Убран отсутп снизу по умолчанию (60px) > Изображения спонсоров перенесены в отдельную папку > Сделана страница для спонсоров #6 > Добавлены ссылки на сайты спонсоров на странице о конференции #9 > Активированы Bootstrap tooltips

--- Mt22KpfuRu/wwwroot/js/site.js
+++ Mt22KpfuRu/wwwroot/js/site.js
@@ -2,3 +2,5 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
